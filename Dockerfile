62559fbcdde7ec11fd9bf0c1d5cf3e9f030a59ab
patricio tula <tula.patricio@gmail.com>
2020-01-19 16:00:20 +0100

    Added usermod to allow docker.sock to connect

--- Dockerfile
+++ Dockerfile
@@ -7,6 +7,6 @@ RUN mkdir -p /tmp/download && \
  mv /tmp/download/docker/docker* /usr/local/bin/ && \
  rm -rf /tmp/download && \
  groupadd -g 999 docker && \
- usermod -aG root jenkins
- 
+ usermod -a -G root jenkins 
+
 user jenkins
--- scripts/jenkins-init.sh
+++ scripts/jenkins-init.sh
@@ -31,9 +31,11 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt-get update
 apt-get install -y docker-ce docker-ce-cli containerd.io
+sudo chmod 666 /var/run/docker.sock
+sudo chown root:docker /var/run/docker.sock
 
 # build docker image
 docker build -t jenkins-docker /tmp/.
 mkdir -p /var/lib/jenkins/jenkins_home
 chown -R 1000:1000 /var/lib/jenkins/jenkins_home
-docker run -p 8080:8080 -p 50000:50000 -v /var/lib/jenkins/jenkins_home:/var/jenkins_home -v /var/jenkins-dockerrun/docker.sock:/var/run/docker.sock --name jenkins -d jenkins-docker 
+docker run -p 8080:8080 -p 50000:50000 -v /var/lib/jenkins/jenkins_home:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock --name jenkins -d jenkins-docker 
